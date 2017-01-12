597570e4abf841e3282c8ae2f53ba7dd86943fd5
johnteee <johnteee@gmail.com>
2017-01-12 16:01:00 +0800

    Add cptoserver.sh . Add install mitmproxy command

---
+++ cptoserver.sh
@@ -0,0 +1,14 @@
+# Usage: cptoserver.sh $* <<< "-pPassword -i YourRSA.id_rsa yourserver@0.0.0.0"
+
+echo -n "ssh "
+read SSH_OPTIONS
+REMOTE_ROOT=/root/remote
+LOCAL_TRANSFER="tar -C / -cf - $*"
+REMOTE_TRANSFER="tar -C $REMOTE_ROOT/ -xvf -"
+ssh $SSH_OPTIONS "mkdir $REMOTE_ROOT"
+$LOCAL_TRANSFER | ssh $SSH_OPTIONS $REMOTE_TRANSFER
+
+# tar -C / -cf - \
+#   opt/widget etc/widget etc/cron.d/widget etc/init.d/widget \
+#   --exclude=opt/widget/local.conf |
+#   ssh otherhost tar -C / -xvf -
