331bcc24e0d8b4a3ee5692e0266eafdcb42e8066
LeeJunYi JohnTeee <johnteee@gmail.com>
2017-09-28 18:26:29 +0800

    Implement git-pull if it already exists.

--- cloneAllBitbucket.sh
+++ cloneAllBitbucket.sh
@@ -2,6 +2,8 @@
 
 echo "cloneAllBitbucket.sh username targetuserorteam"
 
+cwd=$(pwd)
+
 #USER=$1; curl --user ${USER} https://api.bitbucket.org/2.0/repositories/$2 | grep -o '"ssh:[^ ,]\+' | xargs -L1 git clone
 
 curl -u ${1} https://api.bitbucket.org/1.0/users/${2} > repoinfo
@@ -13,6 +15,9 @@ for repo_name in `cat repoinfo | gsed -r 's/("name": )/\n\1/g' | gsed -r 's/"nam
 do
     echo "Cloning " $repo_name
     git clone git@bitbucket.org:${2}/$repo_name.git
+    cd $repo_name
+    git pull
+    cd $cwd
     echo "---"
 done
 
@@ -23,6 +28,9 @@ for repo_name in `cat repoinfo | sed -r 's/("name": )/\n\1/g' | sed -r 's/"name"
 do
     echo "Cloning " $repo_name
     git clone git@bitbucket.org:${2}/$repo_name.git
+    cd $repo_name
+    git pull
+    cd $cwd
     echo "---"
 done
 
