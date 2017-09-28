dc83f22b665bfc7c43caacde447e78ce03eb63b9
LeeJunYi JohnTeee <johnteee@gmail.com>
2017-09-28 18:21:10 +0800

    Add mac osx cases.

--- cloneAllBitbucket.sh
+++ cloneAllBitbucket.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
 
 echo "cloneAllBitbucket.sh username targetuserorteam"
-USER=$1; curl --user ${USER} https://api.bitbucket.org/2.0/repositories/$2 | grep -o '"ssh:[^ ,]\+' | xargs -L1 git clone
+
+#USER=$1; curl --user ${USER} https://api.bitbucket.org/2.0/repositories/$2 | grep -o '"ssh:[^ ,]\+' | xargs -L1 git clone
+
+curl -u ${1} https://api.bitbucket.org/1.0/users/${2} > repoinfo
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+
+for repo_name in `cat repoinfo | gsed -r 's/("name": )/\n\1/g' | gsed -r 's/"name": "(.*)"/\1/' | gsed -e 's/{//' | cut -f1 -d\" | tr '\n' ' '`
+do
+    echo "Cloning " $repo_name
+    git clone git@bitbucket.org:${2}/$repo_name.git
+    echo "---"
+done
+
+else
+        # Unknown.
+
+for repo_name in `cat repoinfo | sed -r 's/("name": )/\n\1/g' | sed -r 's/"name": "(.*)"/\1/' | sed -e 's/{//' | cut -f1 -d\" | tr '\n' ' '`
+do
+    echo "Cloning " $repo_name
+    git clone git@bitbucket.org:${2}/$repo_name.git
+    echo "---"
+done
+
+fi
