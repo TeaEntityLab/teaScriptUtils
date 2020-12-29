e8649ee0df6d4e357f50c1432184d98086a3c7a4
John Lee <johnteee@gmail.com>
2020-12-29 11:17:26 +0800

    Fix android-sdkmanager-bootstrap.sh

--- android-sdkmanager-bootstrap.sh
+++ android-sdkmanager-bootstrap.sh
@@ -1,5 +1,9 @@
 # Credit: caller9@stackoverflow.com
 # Ref: https://stackoverflow.com/questions/60440509/android-command-line-tools-sdkmanager-always-shows-warning-could-not-create-se
 # export ANDROID_HOME=$PWD/android-sdk-linux
-yes | android-sdk-linux/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --licenses
-android-sdk-linux/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
+ANDROID_COMPILE_SDK=29
+ANDROID_BUILD_TOOLS=29.0.3
+ANDROID_SDK_TOOLS=6200805
+
+yes | $ANDROID_HOME/cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --licenses
+$ANDROID_HOME/cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
